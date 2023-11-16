fe9b5b1487422a518394369feb0d324723c8968b
tahaamin2013e <tahaamindob2013@gmail.com>
2023-11-16 15:28:13 +0500

    delete

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -35,7 +35,7 @@ const BlogDetailPage: FC<BlogDetailPageProps> = async ({ params }) => {
       <BackButton />
     <div className='mb-8'>
         <h2 className='text-2xl font-bold my-4'>{post?.title}</h2>
-        <ButtonAction />
+        <ButtonAction id={params.id} />
     </div>
     <span className="badge badge-neutral">{post?.tag.name}</span> 
         <p className='text-slate-700 mt-6'>{post?.content}</p>
--- src/components/ButtonAction.tsx
+++ src/components/ButtonAction.tsx
@@ -1,12 +1,35 @@
 "use client";
+import axios from "axios";
 import { ClipboardEdit, Trash2 } from "lucide-react"
 import Link from "next/link"
+import { useMutation } from "@tanstack/react-query";
+import {FC} from "react"
+import { useRouter } from "next/navigation";
+interface ButtonActionProps {
+  id: string
+}
+const ButtonAction: FC<ButtonActionProps> = ({id}) => {
+  const router = useRouter();
 
-const ButtonAction = () => {
+  const  { mutate: deletePost } = useMutation({
+    mutationFn: async () => {
+      return axios.delete(`/api/posts/${id}`)
+    },
+
+    onError: (error) => {
+      console.error(error);
+    },
+    onSuccess: () => {
+      router.push('/');
+      router.refresh()
+    }
+
+  });
+
   return (
     <div>
         <Link href="/edit/1" className="btn mr-2 btn-success"><ClipboardEdit /> Edit</Link>
-        <button className="btn btn-error"><Trash2 /> Delete</button>
+        <button className="btn btn-error" onClick={() => deletePost()} ><Trash2 /> Delete</button>
     </div>
   )
 }
